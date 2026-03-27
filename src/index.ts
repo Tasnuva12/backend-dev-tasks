7fafd783c8f774c3a61c24ee44e463c3a91efc89
Tasnuva12 <tasnuvamehnaz@iut-dhaka.edu>
2026-03-27 12:27:18 +0600

    Server is running

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
